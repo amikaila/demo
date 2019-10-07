e452b4470b2755e5acb7589c6f5d7fec8b0a733b
Arturas <arturas.lbs@gmail.com>
2019-10-07 22:23:28 +0300

    Homework1

--- src/com/swedbank/javagyk/Main.java
+++ src/com/swedbank/javagyk/Main.java
@@ -1,4 +1,11 @@
 package com.swedbank.javagyk;
 
 public class Main {
+    public static void main(String[] args) {
+        String name = "Arturas";
+        String surname = "Mikaila";
+        String jobRole = "Software Engineer";
+        System.out.println("Hello, I'm " + name + " " + surname + ", I'm " + jobRole);
+
+    }
 }
