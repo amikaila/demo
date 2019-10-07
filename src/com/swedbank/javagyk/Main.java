788a4f4b888ef46036282392452b8f90094100c8
Arturas <arturas.lbs@gmail.com>
2019-10-07 22:09:28 +0300

    Add two args

--- src/com/swedbank/javagyk/Main.java
+++ src/com/swedbank/javagyk/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Java GYK 2019!\n" +
                 "New line");
+
+        System.out.printf("%s%n%s%n", "First arg", "Second arg");
     }
 }
